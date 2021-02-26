b96654b0beec1931359ed9704fc2cfdbf5fc311f
Selena Somvilay <ssomvila@ucsd.edu>
2021-02-25 16:46:03 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'yrz9X61xz4zsQfX3Foa94JBrWixd0Q29';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+    center: [37.7749, -122.4194],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12,
+    zoomControl: false
+});
+
+	// Add marker to the map
+	L.marker([32.87852,-117.2446408]).addTo(map);
 }
